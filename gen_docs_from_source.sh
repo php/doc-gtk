529ff5800a6ea1d659007db47b51e41fba61455b
Christian Weiske <cweiske@php.net>
2005-08-02 16:25:51 +0000

    Automatically generated enum docs. Especially the gdk ones need much hand work, but it was faster than by hand

--- gen_docs_from_source.sh
+++ gen_docs_from_source.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #generates the php-gtk docs from the sources
 #note: the docgen does a chdir to its directory, so the paths have to be relative to php-gtk/generator/
+#there are two parameters: "classonly" and "enumonly"
 
 language=en
 php=php
@@ -14,7 +15,7 @@ fi
 #goto here
 cd "`dirname "$0"`"
 
-if [ ! -f manual ]; then
+if [ ! -d manual/$language/reference ]; then
     mkdir manual
     mkdir manual/$language
     mkdir manual/$language/reference
@@ -24,9 +25,32 @@ if [ ! -f manual ]; then
     mkdir manual/$language/reference/pango
 fi
 
-#generate the docs
-$php -q ../php-gtk/generator/docgen.php -p atk -d ../../php-gtk-doc/manual/$language/reference/atk -s ../../php-gtk-doc/manual/$language/reference/atk ../ext/gtk+/atk.defs
-$php -q ../php-gtk/generator/docgen.php -p gdk -d ../../php-gtk-doc/manual/$language/reference/gdk -s ../../php-gtk-doc/manual/$language/reference/gdk ../ext/gtk+/gdk.defs
-$php -q ../php-gtk/generator/docgen.php -p gtk -d ../../php-gtk-doc/manual/$language/reference/gtk -s ../../php-gtk-doc/manual/$language/reference/gtk ../ext/gtk+/gtk.defs
-$php -q ../php-gtk/generator/docgen.php -p pango -d ../../php-gtk-doc/manual/$language/reference/pango -s ../../php-gtk-doc/manual/$language/reference/pango ../ext/gtk+/pango.defs
+gen_enum=1
+gen_class=1
+if [ $# -gt 0 ]; then
+	if [ $1 == "enumonly" ]; then
+		gen_class=0
+	elif [ $1 == "classonly" ]; then
+		gen_enum=0
+	fi
+fi
 
+#generate the docs
+if [ $gen_class -eq 1 ]; then
+	echo "Generating class docs"
+	$php -q ../php-gtk/generator/docgen.php -p atk -d ../../php-gtk-doc/manual/$language/reference/atk -s ../../php-gtk-doc/manual/$language/reference/atk ../ext/gtk+/atk.defs
+	$php -q ../php-gtk/generator/docgen.php -p gdk -d ../../php-gtk-doc/manual/$language/reference/gdk -s ../../php-gtk-doc/manual/$language/reference/gdk ../ext/gtk+/gdk.defs
+	$php -q ../php-gtk/generator/docgen.php -p gtk -d ../../php-gtk-doc/manual/$language/reference/gtk -s ../../php-gtk-doc/manual/$language/reference/gtk ../ext/gtk+/gtk.defs
+	$php -q ../php-gtk/generator/docgen.php -p pango -d ../../php-gtk-doc/manual/$language/reference/pango -s ../../php-gtk-doc/manual/$language/reference/pango ../ext/gtk+/pango.defs
+fi
+
+#generate enums
+if [ $gen_enum -eq 1 ]; then
+	echo "Generating enumeration docs"
+	$php -q ../php-gtk/generator/docgen_enum.php atk   manual/$language/reference/atk/atkenum.xml
+	$php -q ../php-gtk/generator/docgen_enum.php gdk   manual/$language/reference/gdk/gdkenum.xml
+	$php -q ../php-gtk/generator/docgen_enum.php gtk   manual/$language/reference/gtk/gtkenum.xml
+	$php -q ../php-gtk/generator/docgen_enum.php pango manual/$language/reference/pango/pangoenum.xml
+fi
+
+echo "done"
