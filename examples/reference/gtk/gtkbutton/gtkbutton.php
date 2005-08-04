adf33ad40e68e77dbfc686b62f83062545e7def9
Pablo Dall'Oglio <pablo@php.net>
2005-08-04 19:12:33 +0000

    gtkbutton sample

---
+++ examples/reference/gtk/gtkbutton/gtkbutton.php
@@ -0,0 +1,20 @@
+<?
+
+$window = new GtkWindow;
+$window->set_default_size(120,120);
+
+$vbox = new GtkVBox;
+
+// new Button
+$button = new GtkButton;
+$button->set_label('Icon Test');
+$p = GdkPixbuf::new_from_file('images/publica.png');
+$a = new GtkImage;
+$a->set_from_pixbuf($p);
+$button->set_image($a);
+$vbox->pack_start($button, false, false);
+
+$window->add($vbox);
+$window->show_all();
+gtk::main();
+?>
