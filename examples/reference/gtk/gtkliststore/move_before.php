6d8c159bf03b5778dd65175dcf8feb4f6b41d5f8
Christian Weiske <cweiske@php.net>
2005-12-21 23:37:00 +0000

    Some more examples for the manual

---
+++ examples/reference/gtk/gtkliststore/move_before.php
@@ -0,0 +1,16 @@
+<?php
+$store = new GtkListStore(Gtk::TYPE_STRING, Gtk::TYPE_LONG);
+
+$tokio  = $store->insert(0, array('Tokio', 34100000));
+$mexico = $store->insert(1, array('Mexico city', 22650000));
+$seoul  = $store->insert(2, array('Seoul', 22250000));
+
+//move Seoul before Mexico
+$store->move_before($seoul, $mexico);
+
+//show the list
+function echoRow($store, $path, $iter) {
+    echo $store->get_value($iter, 0) . "\r\n";
+}
+$store->foreach('echoRow');
+?>
---
+++ examples/reference/gtk/gtkliststore/swap.php
@@ -0,0 +1,16 @@
+<?php
+$store = new GtkListStore(Gtk::TYPE_STRING, Gtk::TYPE_LONG);
+
+//insert the rows at different positions
+$tokio  = $store->insert(0, array('Tokio', 34100000));
+$mexico = $store->insert(1, array('Mexico city', 22650000));
+
+//we swap so that mexico is first
+$store->swap($tokio, $mexico);
+
+//show the list
+function echoRow($store, $path, $iter) {
+    echo $store->get_value($iter, 0) . "\r\n";
+}
+$store->foreach('echoRow');
+?>
